3ef8405b0b8de4d2fb1d81ec0e555f3a42cb8deb
Elyas <elyasahmadiiran2022@gmail.com>
2026-06-17 10:54:32 +0430

    removed the devtools

--- src/main.jsx
+++ src/main.jsx
@@ -29,7 +29,7 @@ createRoot(document.getElementById("root")).render(
         <LanguageProvider>
           <AppRoot />
         </LanguageProvider>
-        <ReactQueryDevtools />
+        
       </QueryClientProvider>
     </Theme>
   </StrictMode>,
